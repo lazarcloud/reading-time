d4ae247442bc6c592a46aa3b49a92f1f8eaf4f96
monsieurlazar <dasidasi86@gmail.com>
2025-02-02 16:16:43 +0200

    Update main.go

--- examples/main.go
+++ examples/main.go
@@ -9,7 +9,9 @@ import (
 func main() {
 	estimator := reading_time.StandardEstimator
 
-	time := estimator.CalculateReadingTime(100, 5, 20)
+	time := estimator.CalculateReadingTime(1000, 5, 20)
 
 	fmt.Println(time)
+	fmt.Println(time.Minutes())
+	fmt.Println(time.Seconds())
 }
